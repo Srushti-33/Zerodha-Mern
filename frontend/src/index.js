390d6f43777a300a4355e93a46fba93359c7d011
srushti <srushtikondhalkar25@gmail.com>
2026-01-18 19:14:25 +0530

    Fix import case for Netlify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,12 @@ import './index.css';
 import HomePage from './landing_page/home/HomePage';
 import Signup from './landing_page/signup/Signup';
 import AboutPage from './landing_page/about/AboutPage';
-import PricingPage from './landing_page/Pricing/PricingPage';
+import PricingPage from './landing_page/pricing/PricingPage';
 import SupportPage from './landing_page/support/SupportPage';
 import Navbar from './landing_page/Navbar';
 import Footer from './landing_page/Footer';
 import NotFound from './landing_page/NotFound';
-import ProductsPage from './landing_page/Products/ProductsPage'
+import ProductsPage from './landing_page/products/ProductsPage'
 
 
 
